b095869ffba0dce262368189d173c84396c6a62e
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-09-02 20:39:32 +0700

    Add timestamp logging to getHello method in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('Accessed getHello at', new Date().toISOString());
     return 'Labit UMM 2025';
   }
 }
